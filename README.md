f32df719dda471256616c6034d419c52db26c05a
Cyril Lopez <cyrlop06@hotmail.com>
2021-06-24 16:22:29 +0000

    Initial commit - Ook, world!

---
+++ README.md
@@ -0,0 +1,19 @@
+# Ape mother - Discord bot
+This is a playground bot for discord.
+
+
+
+# Installation
+- Clone repository
+- Install requirements from `requirements.txt`
+- Set your environment variables
+    - `DISCORD_BOT_TOKEN`
+- Run `python main.py`
+
+
+# Usage
+```
+Hey Mother, <your_message>
+```
+## Availalbe commands
+_Coming soon_
---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import discord
+
+
+class Config:
+    command = "Hey Mother, "
+    discord_bot_token = os.environ.get("DISCORD_BOT_TOKEN")
+
+
+config = Config()
+
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print("Logged on as {0}!".format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        data = message.content
+        if data.startswith(config.command):
+            await message.channel.send("Ook, ook ook.")
+
+
+def get_intents():
+    intents = discord.Intents.default()
+    intents.typing = False
+    intents.presences = False
+    intents.messages = True
+    intents.guilds = True
+    intents.reactions = True
+    return intents
+
+
+client = Client(intents=get_intents())
+client.run(config.discord_bot_token)
