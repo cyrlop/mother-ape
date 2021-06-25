e43f8eaba3fec2efddcc0424b5e0d333ad5cbaf7
Cyril Lopez <cyrlop06@hotmail.com>
2021-06-25 21:41:01 +0000

    Improved request to reddit with delay and retries

--- reddit_utils.py
+++ reddit_utils.py
@@ -1,10 +1,19 @@
 import requests
+import time
 
 
 def get_latest_posts_by_flair(sub, flair, limit=20):
     url = f'https://www.reddit.com/r/{sub}/search.json?limit={limit}&sort=new&q=flair_name:"{flair}"'
-    res = requests.get(url)
-    if res.status_code == 200:
-        return res.json()["data"]["children"]
-    elif res.status_code == 429:
-        raise Exception("Too many requests, don't spam!")
+
+    nb_try = 0
+    while nb_try <= 6:
+        nb_try += 1
+        res = requests.get(url)
+
+        if res.status_code == 200:
+            return res.json()["data"]["children"]
+        elif res.status_code == 429:
+            time.sleep(1)
+            continue
+
+    raise Exception("Issue with request: `{res}`")
